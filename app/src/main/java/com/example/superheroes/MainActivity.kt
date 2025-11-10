42a3d6772de43a782891e6cc0bfab38263d6fd7d
laurelwreathh <grindolehop@gmail.com>
2025-11-10 13:33:21 +0900

    done

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.superheroes
 
+import SuperheroesTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.superheroes.ui.theme.SuperheroesTheme
+import com.example.superheroes.model.HeroesRepository
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +25,49 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperheroesTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SuperheroesApp()
+                }
             }
         }
     }
-}
 
+    @Composable
+    fun SuperheroesApp() {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar()
+            }
+        ) {
+
+            val heroes = HeroesRepository.heroes
+            HeroesList(heroes = heroes, contentPadding = it)
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TopAppBar(modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            },
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun SuperHeroesPreview() {
+        SuperheroesTheme {
+            SuperheroesApp()
+        }
+    }
+}
--- app/src/main/java/com/example/superheroes/model/HeroesRepository.kt
+++ app/src/main/java/com/example/superheroes/model/HeroesRepository.kt
@@ -7,32 +7,32 @@ object HeroesRepository {
         Hero(
             nameRes = R.string.hero1,
             descriptionRes = R.string.description1,
-            imageRes = R.drawable.android_superhero1
+            imageRes = R.drawable.sixth
         ),
         Hero(
             nameRes = R.string.hero2,
             descriptionRes = R.string.description2,
-            imageRes = R.drawable.android_superhero2
+            imageRes = R.drawable.first
         ),
         Hero(
             nameRes = R.string.hero3,
             descriptionRes = R.string.description3,
-            imageRes = R.drawable.android_superhero3
+            imageRes = R.drawable.second
         ),
         Hero(
             nameRes = R.string.hero4,
             descriptionRes = R.string.description4,
-            imageRes = R.drawable.android_superhero4
+            imageRes = R.drawable.third
         ),
         Hero(
             nameRes = R.string.hero5,
             descriptionRes = R.string.description5,
-            imageRes = R.drawable.android_superhero5
+            imageRes = R.drawable.fourth
         ),
         Hero(
             nameRes = R.string.hero6,
             descriptionRes = R.string.description6,
-            imageRes = R.drawable.android_superhero6
+            imageRes = R.drawable.fifth
         )
     )
 }
